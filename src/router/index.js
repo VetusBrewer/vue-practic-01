98f88b935ef969aaa71d1a677909e8b28206fe54
Vetus Brewer <angrygantz@yandex.ru>
2021-08-14 21:59:45 +0200

    Create 2 layouts && .

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
+
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      layout: 'main',
+    }
   },
   {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue')
+    path: '/help',
+    name: 'Help',
+    component: () => import('../views/Help.vue'),
+    meta: {
+      layout: 'main',
+    }
+  },
+  {
+    path: '/auth',
+    name: 'Auth',
+    component: () => import('../views/Auth.vue'),
+    meta: {
+      layout: 'auth',
+    }
   }
 ]
 
